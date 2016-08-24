bbc9512a74e9ef6a51cd66ab07bbac52ef004950
Jian Qin <qinjian623@gmail.com>
2016-08-24 23:22:44 +0800

    thing add/list

--- app/assets/javascripts/main.dart
+++ app/assets/javascripts/main.dart
@@ -1,14 +1,84 @@
 import 'dart:html';
 import 'dart:convert';
+import 'dart:async';
+
 InputElement toDoInput;
 UListElement toDoList;
+UListElement thingsList;
+InputElement thingInput;
+var base_url = "http://127.0.0.1:3001/";
 
 void main(){
-  toDoInput = querySelector('#to-do-input');
-  toDoList = querySelector('#to-do-list');
-  toDoInput.onChange.listen(addToDoItem);
+  thingsList = querySelector('#things');
+  thingInput = querySelector('#thing-input');
+  thingInput.onChange.listen(addThing);
+  loadThings();
+}
 
-  loadData();
+void addThing(Event e){
+  var text = thingInput.value;
+  thingInput.value = '';
+  Map m = new Map();
+  m['title'] = text;
+  m['user_id'] = 1;
+  Thing t = new Thing.fromJson(m);
+  t.save();
+  loadThings();
+}
+
+class Thing {
+  String title;
+  int user_id;
+  Thing.fromJson(Map json){
+    this.title = json['title'];
+    this.user_id = json['user_id'];
+  }
+
+  static Future<List<Thing>> all() async{
+    var url = base_url + "things.json";
+    String json_string = await HttpRequest.getString(url);
+    List<Map> json_things = JSON.decode(json_string);
+    List<Thing> ret = new List<Thing>();
+    for(var json_thing in json_things){
+      ret.add(new Thing.fromJson(json_thing));
+    }
+    return ret;
+  }
+
+  void save(){
+    var url = base_url + "things.json";
+    HttpRequest request = new HttpRequest();
+    request.open("POST", url, async: true);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    var mapData = new Map();
+    var thing = new Map();
+
+    thing["title"] = this.title;
+    thing["user_id"] = this.user_id;
+    mapData["thing"] = thing;
+    String jsonData = JSON.encode(mapData); // convert map to String
+    request.send(jsonData); // perform the async POST
+  }
+}
+
+void mountThings(List<Thing> things){
+  thingsList.children.clear();
+  for(var thing in things){
+    thingsList.append(newThingItem(thing));
+  }
+}
+
+LIElement newThingItem(Thing thing){
+  var li = new LIElement();
+  var div = new DivElement();
+  div.text = thing.title;
+  li.children.add(div);
+  return li;
+}
+
+void loadThings(){
+  Thing.all().then(mountThings).catchError((e)=>print(e));
 }
 
 void addToDoItem(Event e){
@@ -19,13 +89,13 @@ void addToDoItem(Event e){
 }
 
 void loadData(){
-  var url = "http://127.0.0.1:3001/users.json";
+  var url = base_url + "users.json";
   // call the web server asynchronously
-  var request = HttpRequest.getString(url).then(onDataLoaded);
+  HttpRequest.getString(url).then(onDataLoaded);
 }
 
 void saveData(value) {
-  var url = "http://127.0.0.1:3001/users.json";
+  var url = base_url + "users.json";
   HttpRequest request = new HttpRequest();
   request.open("POST", url, async: true);
   request.setRequestHeader('Content-Type', 'application/json');
